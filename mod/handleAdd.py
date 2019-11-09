df1687ce0c4bfe11c8547d93fb6334c5d22c64a0
zanjie1999 <zanjie1999@yeah.net>
2019-11-10 01:42:53 +0800

    加入提示

--- mod/handleAdd.py
+++ mod/handleAdd.py
@@ -6,14 +6,15 @@
 
 from config import myId, handleAdd_rule as rule
 
+
 async def handleAdd(bot, context):
-    global myId
     if context['request_type'] == 'group':
         if context['group_id'] in rule.keys():
             comment = context['comment'].lower()
             for i in rule.get(context['group_id']):
                 if i in comment:
                     # 同意申请
+                    print('同意了申请', context['user_id'], '加入', context['group_id'])
                     return {'approve': True}
     elif context['request_type'] == 'friend':
         # 通知小主人处理
