c4e65cec1bbff67d1a1c20058992e7b29e81edb7
zanjie1999 <zanjie1999@yeah.net>
2020-09-11 16:08:02 +0800

    \\r\\n -> \\r

--- mod/admin.py
+++ mod/admin.py
@@ -24,20 +24,20 @@ async def admin(bot, context):
 # 我发消息  方便就写在一起了
 async def admin2at(bot, context):
     global at
-    if context['message'] and '@' == context['message'][0] and '\r\n' in context['message']:
+    if context['message'] and '@' == context['message'][0] and '\r' in context['message']:
         if 'g' == context['message'][1]:
             # 发群消息(指定群号)  @g群号\r\n消息内容
-            msg = context['message'][2:].split('\r\n', 1)
+            msg = context['message'][2:].split('\r', 1)
             await bot.send_group_msg(group_id=msg[0], message=msg[1], auto_escape=False)
             return {'reply': '群消息已发送 @g' + msg[0], 'at_sender': False}
         elif 'q' == context['message'][1]:
             # 发私聊(指定qq号)  @qQQ号\r\n消息内容
-            msg = context['message'][2:].split('\r\n', 1)
+            msg = context['message'][2:].split('\r', 1)
             await bot.send_private_msg(user_id=msg[0], message=msg[1], auto_escape=False)
             return {'reply': '私聊已发送 @q' + msg[0], 'at_sender': False}
         else:
             # 回复at  @xxx\r\n消息内容
-            msg = context['message'][1:].split('\r\n', 1)
+            msg = context['message'][1:].split('\r', 1)
             if msg[0] in at:
                 await bot.send(at[msg[0]], msg[1], at_sender=True, auto_escape=False)
                 # 回复完就删掉啦
@@ -60,7 +60,7 @@ async def at2admin(bot, context):
         # 放进去
         at[tip] = context
         # 提醒我
-        tip = '@g{} 群中被 @{}\n{}({}):\n{}'.format(
+        tip = '@g{} 群中被@{}\n{}({}):\n{}'.format(
             context['group_id'], tip, context['sender']['nickname'], context['user_id'], context['message'])
         print(tip)
         await bot.send_private_msg(user_id=myId, message=tip, auto_escape=False)
